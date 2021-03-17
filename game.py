2872aba17f8d7bd03b4321909b26da2c9555c49b
Chadrick <57962060+MintSquirrel@users.noreply.github.com>
2021-03-17 15:51:36 -0500

    Update game.py

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ Aayush Khadka
 Chadrick Davis
 Muhammad Azam
 
-we're gonna need to import pygame and a bunch of alpha go zero related stuff
+we are gonna need to import pygame and a bunch of alpha go zero related stuff
 also hi
 ```
 
