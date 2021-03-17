aaf483214e162d7ca1f629b87bd22188be79c20e
Chadrick <57962060+MintSquirrel@users.noreply.github.com>
2021-03-17 15:50:45 -0500

    Create game.py

---
+++ game.py
@@ -0,0 +1,15 @@
+'''
+Aayush Khadka
+Chadrick Davis
+Muhammad Azam
+
+we're gonna need to import pygame and a bunch of alpha go zero related stuff
+also hi
+```
+
+#Wow! a test function
+def test():
+  print("Test!")
+
+#Main:
+print("Hello world!")
